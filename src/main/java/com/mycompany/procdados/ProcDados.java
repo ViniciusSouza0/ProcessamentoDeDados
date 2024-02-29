1ff5c1a4004f58772f551244738433ae617d7887
aluno <vinicius.s.silveira.2015@gmail.com>
2024-02-29 20:54:22 -0300

    Procdados

---
+++ src/main/java/com/mycompany/procdados/ProcDados.java
@@ -0,0 +1,81 @@
+package com.mycompany.procdados;
+public class ProcDados {
+    public static void main(String[] args) {
+        
+        
+//Procesamneto de Dados
+
+        //Exemplo 1
+    /*    
+ int x,y;
+x = 5;
+y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+        */
+
+        //Exemplo 2
+  
+        /*
+int x;
+double y;
+
+x = 5;
+y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+      */
+        
+        
+        
+ //Exemplo 3
+ // double se identifica por .0 ou . e um numero
+ // float se identifica por f no final ( b = 6f; )
+ 
+ /*
+ double  b, b2, h, area;
+ b = 6.0;
+ b2 = 8.0;
+ h = 5.0;
+ 
+ area = ( b + b2 ) / 2.0 * h;
+ System.out.println(area);
+ 
+ */
+        
+        
+ //Exemplo 4
+ // casting
+ 
+/*
+int a, b;
+double resultado;
+
+a = 5; 
+b = 2;
+
+ resultado = (double) a / b;  //double está convertendo o valor 
+ */
+
+
+
+//Exemplo 5 --casting (converter) double pra int ou int pra double
+
+/*
+        double a; 
+        int b;
+        
+        a = 5.0;
+        b = (int) a; 
+        
+        System.out.println(b);
+
+*/
+
+
+
+
+    }
+}
